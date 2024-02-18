d1580a8426f1c8403b60df5a5ca45525efb50461
012NaveenG <012guptagkp@gmail.com>
2024-02-18 23:06:09 +0530

    Created message controller and routes

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -4,10 +4,48 @@ import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { AsyncHandler } from "../utils/AsyncHandler.js"
 import { uploadOnCloudinary } from "../utils/Cloudinary.js"
+import { Conversation } from "../models/conversation.model.js"
+
 
 
 const sendMessage = AsyncHandler(async (req, res) => {
-    req.send("Message sent")
+
+    try {
+
+        const { message } = req.body
+        const receiverId = req.params.id
+        const senderId = req.user._id
+
+        let conversation = await Conversation.findOne(
+            { participants: { $all: [senderId, receiverId] } }
+        )
+
+        if (!conversation) {
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId]
+            })
+        }
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            message
+        })
+
+        if (!newMessage) throw new ApiError(500, "Message not sent, Please try again...")
+
+        conversation.messages.push(newMessage) 
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, { newMessage }, "Message sent successfully")
+            )
+
+    } catch (error) {
+        console.log("Error in message controller :: ", error.message);
+        throw new ApiError(500, "Internal server error")
+    }
 
 })
 
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { sendMessage } from "../controllers/message.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
-router.route('/send/:id', sendMessage)
 
+router.route('/send/:id').post(verifyJWT,sendMessage)
 export default router
